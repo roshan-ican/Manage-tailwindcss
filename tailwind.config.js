e7c7732b743b0bbc6028d7aa14254e9df72c6efe
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-05-11 18:33:37 +0530

    all done making the hamburger

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "#f6866a",
+        brightRedSupLight: "	#fdece7",
+        darkBlue: "	#242d52",
+        darkGreyishBlue: "#9095a7",
+        veryDarkBlue: "#1d1e25",
+        veryPaleRed: "#ffefeb",
+        veryLightGrey: "	#fafafa",
+      },
+    },
+  },
+  plugins: [],
+};
